ee140468ff1c04df2538def73011f339b71f12dc
Renato Souza <tttrenato@hotmail.com>
2023-03-30 22:49:27 -0300

    Primeiro projeto, entrada e saida de dados em console

---
+++ Projeto_Primeiro/Projeto_Primeiro/DadosUsuario.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Projeto_Primeiro
+{
+    internal class DadosUsuario
+    {
+        public string Nome { get; set; }
+        public UInt16 Idade { get; set; }
+        public string Documento { get; set; }
+        public string NomeDaRua { get; set; }
+        public UInt32 NumeroDaCasa { get; set; }
+        public string Genero { get; set; }
+
+        public DadosUsuario(string nome, ushort idade, string documento, string nomeDaRua, uint numeroDaCasa, string genero)
+        {
+            Nome = nome;
+            Idade = idade;
+            Documento = documento;
+            NomeDaRua = nomeDaRua;
+            NumeroDaCasa = numeroDaCasa;
+            Genero = genero;
+        }
+
+        public void ExibirInformações()
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+
+            Console.WriteLine($"Olá {Nome}");
+            if (Idade <= 1 && Idade > 0)
+            {
+                Console.WriteLine($"Você tem {Idade} ano de idade");
+            }
+            else
+            {
+                Console.WriteLine($"Você tem {Idade} anos de idade");
+            }
+            Console.WriteLine($"O número do documento informado é: {Documento}");
+            Console.WriteLine($"O nome da sua rua é: {NomeDaRua}");
+            Console.WriteLine($"O número da sua casa é: {NumeroDaCasa}");
+            if (Genero == "F")
+            {
+                Genero = "Feminino";
+            }
+            else if (Genero == "M")
+            {
+                Genero = "Masculino";
+            }
+            Console.WriteLine($"O seu genero é: {Genero}");
+            Console.WriteLine(" ");
+        }
+    }
+}
---
+++ Projeto_Primeiro/Projeto_Primeiro/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Projeto_Primeiro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu nome: ");
+            string Nome = Console.ReadLine();
+            Console.Write("Digite sua idade: ");
+            UInt16 Idade = Convert.ToUInt16(Console.ReadLine());
+            Console.Write("Digite o número do seu documento: ");
+            string Documento = Console.ReadLine();
+            Console.Write("Digite o nome da sua rua: ");
+            string NomeDaRua = Console.ReadLine();
+            Console.Write("Digite o número da sua casa: ");
+            UInt32 NumeroDaCasa = Convert.ToUInt32(Console.ReadLine());
+            Console.Write("Digite seu gênero. Pressione F para feminino e M para masculino: ");
+            string Genero = Console.ReadKey().KeyChar.ToString().ToUpper();
+
+
+            DadosUsuario dados = new DadosUsuario(Nome, Idade, Documento, NomeDaRua, NumeroDaCasa, Genero);
+            dados.ExibirInformações();
+            Console.WriteLine("Pressione qualquer tecla para encerrar...");
+            Console.ReadKey();
+        }
+    }
+}
